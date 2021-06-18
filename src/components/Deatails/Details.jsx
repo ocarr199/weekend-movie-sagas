b62fc446bac8dfabd23ee6893fb8454e7f11d052
Owen Carruth <ocarr199@gmail.com>
2021-06-18 16:33:51 -0500

    image poster, title and details are in description route

--- src/components/Deatails/Details.jsx
+++ src/components/Deatails/Details.jsx
@@ -2,10 +2,13 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function Details() {
     const details = useSelector(store => store.details);
+
+    console.log('IN DETAILS ROUTE',details)
     return (
       <>
-        <h1>Details</h1>
-     
+        <h1>{details.title}</h1>
+     <img src={details.poster}></img>
+     <p>{details.description}</p>
       </>
     );
   }
